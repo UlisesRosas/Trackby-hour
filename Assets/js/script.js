be91cd23922af8798c841e63c85fc14bd7e23121
SLEEPlESS <rosas_u@ymail.com>
2022-01-27 21:17:30 -0800

    added text area and ability to save in to local storage

---
+++ Assets/js/script.js
@@ -0,0 +1,14 @@
+$(".save-btn").on("click",function() {
+    // gets the id from the button that was cliked
+    const time = $(this).attr('id');
+    // gets the value of the text area which is a sibling to the button b/c its in the same div
+    const value = $(this).siblings('.description').val();
+    // stored the key value pair in to local storag
+    localStorage.setItem(time, value)
+});
+
+// added persistant data to each time slot
+for(let i = 9; i <= 17; i++) {
+    // retrievs data from local storage for each slot
+    $(`#${i}`).siblings('.description').val(localStorage.getItem(`${i}`));
+}
